3334cd09e8eb2c40179c3891f4750b039ba8a66e
rajatworlds <rajatsaini736@gmail.com>
2018-07-11 23:49:16 +0530

    first

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from . import forms
-from django.views.genetic import CreateView
+# from django.contrib.auth import login, logout
+from django.views.generic import CreateView
 # Create your views here.
 class SignUp(CreateView):
-	form_class = form.UserCreateForm
-	success_url = reverse_lazy('Login')
-	template_name = 'accounts/signup.html'
+	form_class = forms.UserCreateForm
+	success_url = reverse_lazy("login")
+	template_name = "accounts/signup.html"
 
